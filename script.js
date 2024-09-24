be43350cf9fb022ba92885afcbf853c4f4f0b5b2
Azirool <aziroolfareed31@gmail.com>
2024-09-24 23:11:27 +0800

    Implement hover effect and allow user to change canvas size

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 const container = document.querySelector('.container');
 
-for (let i = 0; i < 16 * 16; i++) {
 
-    const gridBox = document.createElement("div");
-    gridBox.classList.add("grid-box");
+function createGrid(squaresPerSide){
+
+    container.innerHTML = '';
+        
+    const totalSize = 960;
+    const boxSize = totalSize / squaresPerSide; 
     
-    gridBox.onmouseover = function mouseOver(){
-        gridBox.style.backgroundColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
+    for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+
+        const gridBox = document.createElement("div");
+        gridBox.classList.add("grid-box");
+        gridBox.style.width = `${boxSize}px`;
+        gridBox.style.height = `${boxSize}px`;
+
+        gridBox.onmouseover = function mouseOver(){
+            gridBox.style.backgroundColor = "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0");
+        }
+        
+        container.appendChild(gridBox);
     }
-    
-    container.appendChild(gridBox);
+
+    document.getElementById('generateGrid').addEventListener('click', () => {
+        let input = prompt("Enter the canvas size you want");
+        let squaresPerSide = parseInt(input);
+
+        if(!isNaN(squaresPerSide) && squaresPerSide > 0 && squaresPerSide <=100){
+            createGrid(squaresPerSide);
+        } else {
+            alert("Please enter between the range 1 to 100!")
+        }
+    });
 }
+
+createGrid(16);
